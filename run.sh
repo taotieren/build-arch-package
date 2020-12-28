284bda76b1dc39b18ed83cd779deab9281271bdb
taotieren <admin@taotieren.com>
2020-12-28 11:30:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -ex
 
-if [ -z "$INPUT_PKGBUILD" ] || [ -z "$INPUT_OUTDIR" ] || [ -z "$GITHUB_SHA" ]; then
+if [ -z "$INPUT_BUILDING" ] || [ -z "$INPUT_PKGBUILD" ] || [ -z "$INPUT_OUTDIR" ] || [ -z "$GITHUB_SHA" ]; then
     echo 'Missing environment variables'
     exit 1
 fi
 
 # Resolve environment paths
+INPUT_BUILDING="$(eval echo $INPUT_BUILDING)"
 INPUT_PKGBUILD="$(eval echo $INPUT_PKGBUILD)"
 INPUT_OUTDIR="$(eval echo $INPUT_OUTDIR)"
 
 # Get PKGBUILD dir
 PKGBUILD_DIR=$(dirname $(readlink -f $INPUT_PKGBUILD))
 
-# Prepare the environment
 pacman -Syu --noconfirm --noprogressbar --needed base-devel devtools btrfs-progs dbus sudo
 
 dbus-uuidgen --ensure=/etc/machine-id
 
 sed -i "s|MAKEFLAGS=.*|MAKEFLAGS=-j$(nproc)|" /etc/makepkg.conf
-
 useradd -m user
 cd /home/user
 
-# Copy PKGBUILD and *.install scripts
-cp "$PKGBUILD_DIR"/*install ./ || true
+# Copy PKGBUILD and * scripts
+cp "$PKGBUILD_DIR"/* ./ || true
 sed "s|%COMMIT%|$GITHUB_SHA|" "$INPUT_PKGBUILD" > PKGBUILD
 chown user PKGBUILD
 
 # Build the package
-extra-x86_64-build -- -U user
+# See：https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_clean_chroot
+$INPUT_BUILDING -- -U user
 
 # Save the artifacts
 mkdir -p "$INPUT_OUTDIR"
